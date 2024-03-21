c9dde1422ea69ac59f18fa51d9b203a7efc9a22e
david-cruz <davidthylife@gmail.com>
2024-03-21 02:28:50 -0400

    Updated the Style Brian

--- Pathway_pro/app.py
+++ Pathway_pro/app.py
@@ -5,14 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return 'Hello from Flask!'
+    return 'Hello from Flask!'
+
 
 # This is the change I made
 
 @app.route("/home/")
 def home():
-  return render_template("home.html")
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
