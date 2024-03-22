9e6469239156dff1043df8e64cd6898be01b1c60
david-cruz <davidthylife@gmail.com>
2024-03-22 00:24:45 -0400

    Updated app.py for login

--- Pathway_pro/app.py
+++ Pathway_pro/app.py
@@ -14,6 +14,10 @@ def index():
 def home():
     return render_template("home.html")
 
+@app.route("/login/")
+def login():
+    return render_template("login_screen.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
