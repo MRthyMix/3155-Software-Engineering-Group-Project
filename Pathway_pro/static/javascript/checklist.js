ba6669f6e3ea504e5b00a25aeede7058a3e06289
SondashStudios <caiusili2003@gmail.com>
2024-04-01 11:41:00 -0400

    Added checklist.html, checklist.css, and checklist.js

---
+++ Pathway_pro/static/javascript/checklist.js
@@ -0,0 +1,14 @@
+function toggleChecklist(checklistId) {
+    const options = document.getElementById(checklistId);
+    const toggleButton = document.querySelector(`button[onclick="toggleChecklist('${checklistId}')"]`);
+    
+    if (options.style.maxHeight === '0px' || options.style.maxHeight === '') {
+        options.style.maxHeight = '500px'; // Adjust to fit content
+        options.style.opacity = '1';
+        toggleButton.classList.add('expanded');
+    } else {
+        options.style.maxHeight = '0';
+        options.style.opacity = '0';
+        toggleButton.classList.remove('expanded');
+    }
+}
