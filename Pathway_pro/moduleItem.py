acc36ed3d52ae303dcaf75a7525535eb601b5279
kshitijnatu <kshitij.natu@gmail.com>
2024-04-19 16:56:36 -0400

    modified moduleItem.py

--- Pathway_pro/moduleItem.py
+++ Pathway_pro/moduleItem.py
@@ -8,25 +8,40 @@ class ModuleItem:
         self.itemLink = kwargs.get('itemLink')
         self.active = kwargs.get('active')    
         self.ModuleID = kwargs.get('ModuleID')
-    
-    # @staticmethod
-    # def create(ModuleItemID, ItemName, active, ModuleID):
-    #     """
-    #     Creates a new module in the database.
 
-    #     Args:
-    #         ModuleID (str): The ID of the module.
-    #         ModuleName (str): The name of the module.
-    #         active (str): Indicates whether the module is active or not.
-    #     """
-    #     db = get_db()
-    #     db.execute(
-    #         "INSERT INTO ModuleItems (ModuleItemID, ItemName, active, ModuleID) "
-    #         "VALUES (?, ?, ?, ?)",
-    #         (ModuleItemID, ItemName, active, ModuleID),
-    #     )
-    #     db.commit()
-    
+    @staticmethod
+    def create(ModuleItemID, ItemName, itemLink, active, ModuleID):
+        db = get_db()
+        db.execute(
+            "INSERT INTO ModuleItems (ModuleItemID, ItemName, itemLink, active, ModuleID) VALUES (?, ?, ?, ?, ?)",
+            (ModuleItemID, ItemName, itemLink, active, ModuleID)
+        )
+        db.commit()
+
+    @staticmethod
+    def get(ModuleItemID):
+        db = get_db()
+        moduleItem = db.execute(
+            "SELECT * FROM ModuleItems WHERE ModuleItemID = ?", (ModuleItemID,)
+        ).fetchone()
+        if not moduleItem:
+            return None
+        return ModuleItem(ModuleItemID=moduleItem[0], ItemName=moduleItem[1], itemLink=moduleItem[2], active=moduleItem[3], ModuleID=moduleItem[4])
+
+    @staticmethod
+    def delete(ModuleItemID):
+        db = get_db()
+        db.execute("DELETE FROM ModuleItems WHERE ModuleItemID = ?", (ModuleItemID,))
+        db.commit()
+
+    @staticmethod
+    def update(ModuleItemID, ItemName, itemLink, active, ModuleID):
+        db = get_db()
+        db.execute(
+            "UPDATE ModuleItems SET ItemName = ?, itemLink = ?, active = ?, ModuleID = ? WHERE ModuleItemID = ?",
+            (ItemName, itemLink, active, ModuleID, ModuleItemID)
+        )
+        db.commit()
     
     @staticmethod
     def getByModuleID(module_id): 
