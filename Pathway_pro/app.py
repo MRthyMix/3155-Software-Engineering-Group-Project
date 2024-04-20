801cb51902bb55febd19105263fb05748133cc74
david-cruz <davidthylife@gmail.com>
2024-04-20 12:49:48 -0400

    add index.html

--- Pathway_pro/app.py
+++ Pathway_pro/app.py
@@ -53,7 +53,7 @@ client = WebApplicationClient(GOOGLE_CLIENT_ID)
 def load_user(user_id):
     return User.get(user_id)
 
-@app.route("/")
+@app.route("/index.html")
 def index():
     return render_template("home.html")
 
